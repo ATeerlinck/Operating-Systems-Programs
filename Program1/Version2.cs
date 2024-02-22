c1eac868d26d80df2565f99f3d3fac1493cf9bd2
Alex Teerlinck <ateerlinckwork@gmail.com>
2024-02-22 13:29:15 -0600

    Version 2 ready for testing

--- Program1/Version2.cs
+++ Program1/Version2.cs
@@ -17,7 +17,8 @@ namespace Program1
     public class Version2
     {
         // Declare any class/instance variables that you need here.
-
+        LinkedList<Version2PCB> pcbArray;
+        int pcbCount = 0;
         /**
         * Default constructor. Use this to allocate (if needed) and
         * initialize the PCB array, create the PCB for process 0, and do
@@ -25,7 +26,8 @@ namespace Program1
         */
         public Version2()
         {
-
+            pcbArray = new LinkedList<Version2PCB>();
+            create(-1);
         }
 
 
@@ -38,14 +40,29 @@ namespace Program1
             // If parentPid is not in the process hierarchy, do nothing; 
             // your code may return an error code or message in this case,
             // but it should not halt
-
+            if(!pcbArray.Where(p => p.processID == targetPid).Exists() && parentPid > 0){
+                return 1;
+            }
             // Assuming you've found the PCB for parentPid in the PCB array:
             // 1. Allocate and initialize a free PCB object from the array
             //    of PCB objects
-
+            Version2PCB newPCB = new Version2PCB(parentPid, (pcbArray.contains(null) ? pcbArray.Where(p => p == null).First()/*need location number in pcbArray*/: pcbCount))
+            
             // 2. Connect the new PCB object to its parent, its older
             //    sibling (if any), and its younger sibling (if any)
-
+            if(pcbArray.ElementAt(parentPid).getFirstChild() == null) pcbArray.ElementAt(parentPid).setFirstChild(newPCB.processID);
+            else{
+                Version2PCB sibling = pcbArray.ElementAt(pcbArray.ElementAt(parentPid).getFirstChild());
+                while(pcbArray.ElementAt(sibling).getYoungerSibling() != null) sibling = pcbArray.ElementAt(subling.getYoungerSibling)
+                sibling.setYoungerSibling(newPCB.getProcessID());
+                newPCB.setOlderSibling(sibling.processID());
+            }
+            if(pcbArray.contains(null)) pcbArray.Where(p => p.processID == parentPid).First().Value = newPCB);
+            else {
+                pcbArray.AddLast(newPCB);
+                pcbCount++;
+            }
+            
             // You can decide what the return value(s), if any, should be.
             // If you change the return type/value(s), update the XML.
             return 0; // often means "success" or "terminated normally"
@@ -60,17 +77,21 @@ namespace Program1
             // If targetPid is not in the process hierarchy, do nothing; 
             // your code may return an error code or message in this case,
             // but it should not halt
-
+            if(!pcbArray.Where(p => p.processID == targetPid).Exists){
+                return 1;
+            }
             // Assuming you've found the PCB for targetPid in the PCB array:
             // 1. Recursively destroy all descendants of targetPid, if it
             //    has any, and mark their PCBs as "free" in the PCB array 
             //    (i.e., deallocate them)
-
+            Version2PCB targetPcb = pcbArray.Where(p => p.processID == targetPid).First();
             // 2. Remove targetPid from its parent's list of children
-
+            pcbArray.ElementAt(targetPcb.getOlderSibling()).setYoungerSibling(targetPcb.getYoungerSibling());
+            pcbArray.ElementAt(targetPcb.setYoungerSibling()).getOlderSibling(targetPcb.getOlderSibling());
+            if(pcbArray.ElementAt(targetPcb.getParent()).getFirstChild() == targetPcb.processID()) pcbArray.ElementAt(targetPcb.getParent()).setFirstChild(targetPcb.getYoungerSibling());
             // 3. Deallocate targetPid's PCB and mark its PCB array entry
             //    as "free"
-
+            pcbArray.Find(targetPcb).Value = null;
             // You can decide what the return value(s), if any, should be.
             // If you change the return type/value(s), update the XML.
             return 0; // often means "success" or "terminated normally"
@@ -88,7 +109,18 @@ namespace Program1
         */
         void showProcessInfo()
         {
-
+            string children = ""
+            foreach(Version2PCB p in pcbArray){
+                if(p.firstChild != null){
+                    children = "" + pcbArray.ElementAt(pcbArray.ElementAt(p).getFirstChild());
+                    Version2PCB sibling = pcbArray.ElementAt(pcbArray.ElementAt(p).getFirstChild());
+                    while(sibling != null){
+                        children += ", " + pcbArray.ElementAt(pcbArray.ElementAt(p).getFirstChild();
+                        sibling = pcbArray.ElementAt(pcbArray.ElementAt(sibling).getYoungerSibling());
+                    }
+                }
+                Console.Write(p != null ? "Process " + p.processID + ": parent is " + p.parent + " and " + ((p.firstChild != null) ? "children are " + children + "\n" : "has no children\n"): ""); 
+            }
         }
 
         /* If you need or want more methods, feel free to add them. */
