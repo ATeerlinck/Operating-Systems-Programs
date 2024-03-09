6ac9d51e47aed2418db1416c297662b35e44f9f6
Alex Teerlinck <ateerlinckwork@gmail.com>
2024-03-09 00:43:24 -0600

    program 2 init

---
+++ Program2/Producer.cs
@@ -0,0 +1,35 @@
+/* COMPSCI 424 Program 2
+ * Name:
+ */
+namespace Program2
+{
+    /**
+     * A class for the producer thread.
+     */
+    public class Producer
+    {
+        // Declare any class/instance variables that you need here.
+
+        /**
+         * Default constructor. Use this to do any allocation and
+         * initialization that is needed. If you need more constructors
+         * than this, feel free to add other constructors.
+         */
+        public Producer()
+        {
+
+        }
+
+        /**
+         * Essentially the "main method" for this thread. Call this 
+		 * method (or another method) when you start the producer 
+         * thread. May accept arguments; must return void.
+         */
+        public void Run()
+        {
+
+        }
+
+        /* If you need or want more methods, feel free to add them. */
+    }
+}
